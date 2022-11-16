4c0a0585fc0fcb9335e08f93bc6fd2a10a33df13
Alain Vallejo <alain.vallejo@laposte.net>
2022-11-16 11:55:32 -0400

    base de données

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_185428) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_152826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_185428) do
     t.datetime "updated_at", null: false
     t.string "color"
     t.string "name"
+    t.string "url"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,38 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-require 'faker'
-
-# puts 'Creating 10 fake wigs...'
-types = ["natural", "artificial"]
-names = Name.create([{ name: "MADISON" }, { name: "ANASTASIA" }, { name: "CAROLINE" }, { name: "JANICE" }, { name: "IVY" }, { name: "KIM" }, { name: "LORETTA" }, { name: "CHLOÉ" }, { name: "ESTELLE" }, { name: "ELISA" }])
-sizes = Size.create([{ size: "Small" }, { size: "Medium" }, { size: "Large" }])
-colors = Color.create([{ color: "Black" }, { color: "Brown" }])
-hair_styles = Hair_style.create([{ hair_style: "Noir" }, { hair_style: "Lord of the Rings" }])
-hair_types = Hair_type.create([{ hair_type: "Star Wars" }, { hair_type: "Lord of the Rings" }])
-
-Wig.create(name: "Luke", movie: movies.first)
-
 Wig.destroy_all
 
-10.times do
-  Wig.create!(
-    name: Faker::Name.middle_name,
-    size: Faker::Measurement.height,
-    color: Faker::Color.hex_color,
-    hair_style: Faker::Music.genre,
-    hair_type: types.sample
+# puts 'Creating 10 fake wigs...'
 
-  )
-end
+# names = Name.create([{ name: "MADISON" }, { name: "ANASTASIA" }, { name: "CAROLINE" }, { name: "JANICE" }, { name: "IVY" }, { name: "KIM" }, { name: "LORETTA" }, { name: "CHLOÉ" }, { name: "ESTELLE" }, { name: "ELISA" }])
+# sizes = Size.create([{ size: "small" }, { size: "medium" }, { size: "large" }])
+# colors = Color.create([{ color: "black" }, { color: "brown" }])
+# hair_styles = Hair_style.create([{ hair_style: "smooth" }, { hair_style: "curly" }, { hair_style: "wavy" }])
+# hair_types = Hair_type.create([{ hair_type: "natural" }, { hair_type: "artificial" }])
+
+wig_a = Wig.create!(name: "MADISON", size: "small", color: "black", hair_style: "smooth", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544598/development/soaa145-01_ei77gr.jpg")
+wig_b = Wig.create!(name: "ELISE", size: "large", color: "black", hair_style: "wavy", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544550/development/soaa185-01_ugzm0k.jpg")
+wig_c = Wig.create!(name: "ANASTASIA", size: "medium", color: "brown", hair_style: "curly", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544509/development/soaa135-01_alglk1.jpg")
+wig_d = Wig.create!(name: "CAROLINE", size: "large", color: "black", hair_style: "smooth", hair_type: "artificial", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544598/development/soaa132-01_f8txfz.jpg")
+wig_e = Wig.create!(name: "JANICE", size: "large", color: "brown", hair_style: "curly", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544509/development/soaa197-01_medlud.jpg")
+wig_f = Wig.create!(name: "IVY", size: "small", color: "brown", hair_style: "wavy", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544509/development/soaa138-01_xhnqhl.jpg")
+wig_g = Wig.create!(name: "KIM", size: "large", color: "black", hair_style: "wavy", hair_type: "artificiall", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544548/development/soaa11-01_jkuvwx.jpg")
+wig_h = Wig.create!(name: "LORETTA", size: "large", color: "black", hair_style: "smooth", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544548/development/soaa113-01_x8cfhs.jpg")
+wig_j = Wig.create!(name: "CHLOE", size: "medium", color: "brown", hair_style: "smooth", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544550/development/soaa193-01_osupvx.jpg")
+wig_k = Wig.create!(name: "ESTELLE", size: "small", color: "brown", hair_style: "curly", hair_type: "artificial", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544508/development/soaa153-01_wzq8zf.jpg")
+wig_l = Wig.create!(name: "ELISA", size: "large", color: "black", hair_style: "curly", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544549/development/soaa142-01_uwn5qz.jpg")
+
+# Wig.create(name: names.sammple, size: size.sample, color: color.sample, hair_styles: hair_style.sample, hair_type: hair_type.sammple)
+
+
+# 10.times do
+#   Wig.create!(
+#     name: Faker::Name.middle_name,
+#     size: Faker::Measurement.height,
+#     color: Faker::Color.hex_color,
+#     hair_style: Faker::Music.genre,
+#     hair_type: types.sample
+#   )
+# end
 puts 'Finished!'
