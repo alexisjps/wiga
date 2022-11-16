601ddbe9a2cebc2e474c31ce64f62460659fe654
Alain Vallejo <alain.vallejo@laposte.net>
2022-11-16 13:54:58 -0400

    seeds & index

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,6 @@
 
 Wig.destroy_all
 
-# puts 'Creating 10 fake wigs...'
-
-# names = Name.create([{ name: "MADISON" }, { name: "ANASTASIA" }, { name: "CAROLINE" }, { name: "JANICE" }, { name: "IVY" }, { name: "KIM" }, { name: "LORETTA" }, { name: "CHLOÉ" }, { name: "ESTELLE" }, { name: "ELISA" }])
-# sizes = Size.create([{ size: "small" }, { size: "medium" }, { size: "large" }])
-# colors = Color.create([{ color: "black" }, { color: "brown" }])
-# hair_styles = Hair_style.create([{ hair_style: "smooth" }, { hair_style: "curly" }, { hair_style: "wavy" }])
-# hair_types = Hair_type.create([{ hair_type: "natural" }, { hair_type: "artificial" }])
-
 wig_a = Wig.create!(name: "MADISON", size: "small", color: "black", hair_style: "smooth", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544598/development/soaa145-01_ei77gr.jpg")
 wig_b = Wig.create!(name: "ELISE", size: "large", color: "black", hair_style: "wavy", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544550/development/soaa185-01_ugzm0k.jpg")
 wig_c = Wig.create!(name: "ANASTASIA", size: "medium", color: "brown", hair_style: "curly", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544509/development/soaa135-01_alglk1.jpg")
@@ -28,16 +20,4 @@ wig_j = Wig.create!(name: "CHLOE", size: "medium", color: "brown", hair_style: "
 wig_k = Wig.create!(name: "ESTELLE", size: "small", color: "brown", hair_style: "curly", hair_type: "artificial", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544508/development/soaa153-01_wzq8zf.jpg")
 wig_l = Wig.create!(name: "ELISA", size: "large", color: "black", hair_style: "curly", hair_type: "natural", url:"https://res.cloudinary.com/dcjj6hxol/image/upload/v1668544549/development/soaa142-01_uwn5qz.jpg")
 
-# Wig.create(name: names.sammple, size: size.sample, color: color.sample, hair_styles: hair_style.sample, hair_type: hair_type.sammple)
-
-
-# 10.times do
-#   Wig.create!(
-#     name: Faker::Name.middle_name,
-#     size: Faker::Measurement.height,
-#     color: Faker::Color.hex_color,
-#     hair_style: Faker::Music.genre,
-#     hair_type: types.sample
-#   )
-# end
 puts 'Finished!'
