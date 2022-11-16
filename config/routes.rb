050f318d1dfe3f12d40644e3751fcbfbce9df8ba
Morgann Gabourg <morgann9d97@gmail.com>
2022-11-16 16:06:25 -0400

    home page et navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :wigs
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in 
 
   # Defines the root path route ("/")
   # root "articles#index"
