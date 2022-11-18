152ba3178162d347f6ecd13371ec56705fc1f52c
Alain Vallejo <alain.vallejo@laposte.net>
2022-11-18 08:15:30 -0400

    test

--- db/migrate/20221117194705_add_wig_id_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class AddWigIdToBookings < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :bookings, :wig, null: false, foreign_key: true
-  end
-end
--- db/migrate/old_remove_user_id_from_bookings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromBookings < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :bookings, :users_id
-  end
-end
